96585c76218fe8297072c0b307dbb696874632da
chaiyanchen <chaiyanchen@baidu.com>
2019-08-29 14:06:36 +0800

    update ts2php

--- a.js
+++
@@ -1,6 +0,0 @@
-ts2php = require('./dist/index.js')
-
-const file = {path: '/Users/chaiyanchen/Documents/code/toptip/src/index.ts', push: () => false}
-ts2php.compile(file, null, () => {
-    console.log(file.contents);
-})
